1d95b40dd162f911670b5c19a31bcb681e22e1a8
Miki <Mori>
2022-09-15 19:50:10 +0900

    intent with viewpost

---
+++ app/src/main/java/com/example/tgh/ViewpostActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tgh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewpostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.viewpost)
+    }
+}
