3aab08b545f328845d883e1aab461125dc08a96c
margaret <magara3313@gmail.com>
2022-09-16 09:20:44 +0900

    for the first firestore test

--- app/src/main/java/com/example/tgh/MainActivity.kt
+++ app/src/main/java/com/example/tgh/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.tgh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tgh.data.UserData
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,5 +19,31 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     // 12~
-    // from website for "view binding"
+    // from web site for "view binding"
 }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Firestoreをインスタンス化
+        val db = Firebase.firestore
+
+        // ボタンを押したときの処理
+        binding.addButton.setOnClickListener {
+
+            // Taskをインスタンス化
+            val task = UserData(
+                title = binding.titleEditText.text.toString(),
+            )
+
+            db.collection("users")
+                .add(task)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(ADD_TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.w(ADD_TAG, "Error adding document", e)
+                }
+        }
+    }
