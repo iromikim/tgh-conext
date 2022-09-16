330be7d953e5bb2640265077f9e6eb63e76e3af6
Miki <Mori>
2022-09-16 15:20:51 +0900

    textview in mapcloseup

--- app/src/main/java/com/example/tgh/MapCloseupActivity.kt
+++ app/src/main/java/com/example/tgh/MapCloseupActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.widget.AppCompatImageButton
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -14,26 +15,25 @@ class MapCloseupActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.map_closeup)
 
-       /** val btnBack : Button = findViewById(R.id.buttonforreturn)
-        //戻るボタン（アクティビティの終了）
-        btnBack.setOnClickListener {
-            finish()
-        }
-       */
-
         Firebase.firestore.collection("UserData")
             .get()
             .addOnSuccessListener { result ->
-                for (document in result) {
-                    Log.d("Post", document.data.get("contents").toString())
-                    Log.d("Post", document.data.get("contents").toString())
-                    Log.d("Post",  document.data.get("contents").toString() )
-                    Log.d("Post",  document.data.get("contents").toString() )
-                    Log.d("Post",  document.data.get("contents").toString() )
+                val contents = result.documents.map { it.data?.get("contents").toString() }
+                val posts = listOf<TextView>(
+                    findViewById<TextView>(R.id.bubble1text),
+                    findViewById<TextView>(R.id.bubble2text),
+                    findViewById<TextView>(R.id.bubble3text),
+                    findViewById<TextView>(R.id.bubble4text),
+                    findViewById<TextView>(R.id.bubble5text),
+                )
+                tags.forEachIndexed { index, s ->
+                    if (index >= bubbletexts.size) return@forEachIndexed
+                    bubbletexts[index].text = s
                 }
             }
 
-       val btnToReturn: Button =findViewById(R.id.buttonforreturn)
+
+    val btnToReturn: Button =findViewById(R.id.buttonforreturn)
         btnToReturn.setOnClickListener {
             val intent = Intent(this,MapActivity::class.java)
             startActivity(intent)
