469999d5fce66211bedc4a1a7d95a453c0550005
margaret <magara3313@gmail.com>
2022-09-16 15:35:06 +0900

    post_undone

--- app/src/main/java/com/example/tgh/PostActivity.kt
+++ app/src/main/java/com/example/tgh/PostActivity.kt
@@ -9,10 +9,16 @@ import android.util.Log
 import android.view.MotionEvent
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
+import android.widget.EditText
 import android.widget.LinearLayout
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.tgh.databinding.ActivityMainBinding
+import com.google.firebase.firestore.DocumentId
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.core.UserData
+import com.google.firebase.firestore.IgnoreExtraProperties
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
 
 class PostActivity : AppCompatActivity() {
     private lateinit var container: LinearLayout
@@ -30,47 +36,51 @@ class PostActivity : AppCompatActivity() {
         // InputMethodManagerを取得
         inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
 
-        val btnToPost: Button =findViewById(R.id.post)
+        val btnToPost: Button = findViewById(R.id.post)
         btnToPost.setOnClickListener {
-            val intent = Intent(this,MapActivity::class.java)
+            val intent = Intent(this, MapActivity::class.java)
             startActivity(intent)
         }
 
-        val btnToReturn: Button =findViewById(R.id.buttonforreturn)
+        val btnToReturn: Button = findViewById(R.id.buttonforreturn)
         btnToReturn.setOnClickListener {
-            val intent = Intent(this,MapActivity::class.java)
+            val intent = Intent(this, MapActivity::class.java)
             startActivity(intent)
+        }
+
+
+        //ボタンを押したとき
+        val btnStart: Button = findViewById(R.id.post)
+        btnStart.setOnClickListener {
+           val user=UserData(id="testuser", contents ="test",tag="design")
+            //setvalueを使いたい
+            Firebase.firestore.collection("UserData")
+                .add(user)
+                .addOnSuccessListener { Log.d("TAG", "DocumentSnapshot successfully updated!") }
+                .addOnFailureListener { e -> Log.w("TAG", "Error updating document", e) }
+
+            //database.child("UserData").child(userId).setValue(UserData)
+
+
         }
 
     }
+
+
+
 
     // 画面タップ時に呼ばれる
     override fun onTouchEvent(event: MotionEvent): Boolean {
 
         // キーボードを隠す
-        inputMethodManager.hideSoftInputFromWindow(container.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS)
+        inputMethodManager.hideSoftInputFromWindow(
+            container.getWindowToken(),
+            InputMethodManager.HIDE_NOT_ALWAYS
+        )
 
         // 背景にフォーカスを移す
         container.requestFocus()
 
         return false
-    }
-
-    private val database : FirebaseFirestore get() = FirebaseFirestore.getInstance()
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        //binding =
-
-        //ボタンを押したとき
-        binding.post.setOnClickListener{
-
-            val task = UserData(
-                title
-            )
-            db.collection("Users")
-                .add(task)
-
-                }
-        }
     }
 }
---
+++ app/src/main/java/com/example/tgh/UserData.kt
@@ -0,0 +1,3 @@
+package com.example.tgh
+
+data class UserData(val id: String?=null, val contents: String?=null, val tag: String?=null)
